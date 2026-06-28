de54d2ede249dcfa8d34def07356c886fc159b20
david <dchordigian.tech@gmail.com>
2026-06-28 16:28:03 -0700

    feat: add asteroid splitting mechanic
    - Add Asteroid.split() method that destroys the current asteroid and spawns two smaller ones moving in randomized directions
    - Smaller asteroids move 20% faster than the parent
    - Log asteroid_split events via log_event()
    - Replace asteroid.kill() with asteroid.split() in game loop

--- asteroid.py
+++ asteroid.py
@@ -1,10 +1,27 @@
 import pygame
+import random
+from logger import log_event
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x:float, y:float, radius:float) -> None:
         super().__init__(x, y, radius)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return 
+        else:
+            log_event("asteroid_split")
+            random_angle = random.uniform(20, 50)
+            first_vector = self.velocity.rotate(random_angle)
+            second_vector = self.velocity.rotate(-random_angle)
+            split_radius = self.radius - ASTEROID_MIN_RADIUS
+            new_asteroid_one = Asteroid(self.position.x, self.position.y, split_radius)
+            new_asteroid_two = Asteroid(self.position.x, self.position.y, split_radius)
+            new_asteroid_one.velocity = first_vector * 1.2
+            new_asteroid_two.velocity = second_vector * 1.2
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
